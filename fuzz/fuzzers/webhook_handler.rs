9f0ad9f5b7c2a864be133345aa39067228f8163b
Brian Martin <brayniac@gmail.com>
2017-06-03 15:20:08 -0700

    fuzz: cargo-fuzz support
    - add fuzzing
    - add travis fuzz testing
    - fix handling of malformed JSON payloads

---
+++ fuzz/fuzzers/webhook_handler.rs
@@ -0,0 +1,22 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+#[macro_use]
+extern crate log;
+extern crate getopts;
+extern crate json;
+extern crate tic;
+extern crate tiny_http;
+
+#[path = "../../src/metrics.rs"]
+mod metrics;
+
+#[path = "../../src/webhook.rs"]
+mod webhook;
+
+fuzz_target!(|data: &[u8]| {
+    // fuzzed code goes here
+    if let Ok(s) = String::from_utf8(data.to_vec()) {
+    	webhook::handle_payload(&s);
+    }
+});
