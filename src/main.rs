0d42a96bd9e1527972c27805517067b829163908
Brian Martin <brayniac@gmail.com>
2017-06-03 15:08:22 -0700

    refactor: webhook
    - split http webhook server out

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate tiny_http;
 mod logging;
 mod metrics;
 mod options;
+mod webhook;
 
 use logging::set_log_level;
 use metrics::Metric;
@@ -31,44 +32,11 @@ fn main() {
 
     // initialize http listener
     let http = options.opt_str("http").unwrap_or("0.0.0.0:4567".to_owned());
-    let server = tiny_http::Server::http(&http).unwrap();
-    info!("listening HTTP {}", &http);
-
-    // run the server
-    loop {
-        if let Ok(Some(request)) = server.try_recv() {
-            debug!("handle request");
-            let t0 = clock.counter();
-            handle_http(request);
-            let t1 = clock.counter();
-            let _ = stats.send(Sample::new(t0, t1, Metric::Request));
-        }
-    }
-}
-
-// actually handle the http request
-fn handle_http(mut request: Request) {
-    let response = match *request.method() {
-        Method::Post => {
-            match request.url() {
-                "/payload" => {
-                    info!("payload received");
-                    let mut content = String::new();
-                    request.as_reader().read_to_string(&mut content).unwrap();
-                    handle_payload(&content)
-                }
-                _ => Response::empty(404),
-            }
-        }
-        _ => Response::empty(405),
-    };
-
-    let _ = request.respond(response);
-}
-
-// parse payload
-fn handle_payload(payload: &str) -> Response<io::Empty> {
-    info!("handle payload");
-    let parsed = json::parse(payload).unwrap();
-    Response::empty(200)
+    let mut server = webhook::Server::configure()
+                        .listen(http)
+                        .clock(clock)
+                        .stats(stats)
+                        .build()
+                        .unwrap();
+    server.run();
 }
