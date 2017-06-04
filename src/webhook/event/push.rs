ee751cdc9cc9c56eda7091272464f2034069c198
Brian Martin <brayniac@gmail.com>
2017-06-04 14:41:07 -0700

    feature: push parsing
    - more parsing
    - add test

--- src/webhook/event/push.rs
+++ src/webhook/event/push.rs
@@ -3,21 +3,41 @@ extern crate json;
 #[derive(Clone, Debug)]
 pub struct Push {
     git_ref: String,
+    repo: String,
 }
 
 impl Push {
     pub fn from_str(payload: &str) -> Result<Push, &'static str> {
         debug!("parse push");
         if let Ok(parsed) = json::parse(payload) {
-            if let Some(git_ref) = parsed["ref"].as_str() {
-                let push = Push { git_ref: git_ref.to_owned() };
-                debug!("{:?}", push);
-                Ok(push)
-            } else {
-                Err("malformed event")
+            let git_ref = parsed["ref"].as_str();
+            let repo = parsed["repository"]["full_name"].as_str();
+            if git_ref.is_none() {
+                return Err("missing 'ref'");
             }
+            if repo.is_none() {
+                return Err("missing 'repository.full_name'");
+            }
+            let push = Push {
+                git_ref: git_ref.unwrap().to_owned(),
+                repo: repo.unwrap().to_owned(),
+            };
+            debug!("{:?}", push);
+            Ok(push)
         } else {
             Err("malformed json")
         }
+    }
+}
+
+mod test {
+    use webhook::event::push::Push;
+
+    #[test]
+    fn test_parse() {
+        let payload = include_str!("push.json");
+        let push = Push::from_str(payload).unwrap();
+        assert_eq!(push.git_ref, "refs/heads/changes");
+        assert_eq!(push.repo, "baxterthehacker/public-repo");
     }
 }
