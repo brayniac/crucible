96c5b7ca3ebf21dbe17306c6f90aea1970062207
Brian Martin <brayniac@gmail.com>
2017-06-04 11:11:56 -0700

    feature: add event parsing (#9)
    * feature: add event parsing
    
    - parse event types, partially
    
    * rustfmt

--- fuzz/fuzzers/webhook_pull_request.rs
+++ fuzz/fuzzers/webhook_pull_request.rs
@@ -15,8 +15,8 @@ mod metrics;
 mod webhook;
 
 fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
-    if let Ok(s) = String::from_utf8(data.to_vec()) {
-    	webhook::handle_payload(&s);
-    }
-});
+                 // fuzzed code goes here
+                 if let Ok(s) = String::from_utf8(data.to_vec()) {
+                     webhook::handle_pull_request(&s);
+                 }
+             });
