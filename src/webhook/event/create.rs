dd763d7be99de6c4eb31ebee90355fe3e5c7b80b
Brian Martin <brayniac@gmail.com>
2017-06-04 15:26:00 -0700

    feature: more parsing
    - parsing pull_request
    - parsing create
    - add tests
    
    add test file
    
    feature: create parsing
    
    test: add json

--- src/webhook/event/create.rs
+++ src/webhook/event/create.rs
@@ -3,21 +3,44 @@ extern crate json;
 #[derive(Clone, Debug)]
 pub struct Create {
     git_ref: String,
+    ref_type: String,
 }
 
 impl Create {
     pub fn from_str(payload: &str) -> Result<Create, &'static str> {
-        debug!("parse create");
+        debug!("parse");
         if let Ok(parsed) = json::parse(payload) {
-            if let Some(git_ref) = parsed["ref"].as_str() {
-                let create = Create { git_ref: git_ref.to_owned() };
-                debug!("{:?}", create);
-                Ok(create)
-            } else {
-                Err("malformed event")
+            let git_ref = parsed["ref"].as_str();
+            let ref_type = parsed["ref_type"].as_str();
+
+            if git_ref.is_none() {
+                return Err("missing 'ref'");
             }
+            if ref_type.is_none() {
+                return Err("missing 'ref_type'");
+            }
+
+            let event = Create {
+                git_ref: git_ref.unwrap().to_owned(),
+                ref_type: ref_type.unwrap().to_owned(),
+            };
+
+            debug!("{:?}", event);
+            Ok(event)
         } else {
             Err("malformed json")
         }
+    }
+}
+
+mod test {
+    use super::Create;
+
+    #[test]
+    fn test_parse() {
+        let payload = include_str!("create.json");
+        let event = Create::from_str(payload).unwrap();
+        assert_eq!(event.git_ref, "0.0.1");
+        assert_eq!(event.ref_type, "tag");
     }
 }
--- src/webhook/event/pull_request.rs
+++ src/webhook/event/pull_request.rs
@@ -3,21 +3,44 @@ extern crate json;
 #[derive(Clone, Debug)]
 pub struct PullRequest {
     action: String,
+    number: u64,
 }
 
 impl PullRequest {
     pub fn from_str(payload: &str) -> Result<PullRequest, &'static str> {
         debug!("parse pull_request");
         if let Ok(parsed) = json::parse(payload) {
-            if let Some(action) = parsed["action"].as_str() {
-                let pr = PullRequest { action: action.to_owned() };
-                debug!("{:?}", pr);
-                Ok(pr)
-            } else {
-                Err("malformed event")
+            let action = parsed["action"].as_str();
+            let number = parsed["number"].as_u64();
+
+            if action.is_none() {
+                return Err("missing 'action'");
             }
+            if number.is_none() {
+                return Err("missing 'number'");
+            }
+
+            let event = PullRequest {
+                action: action.unwrap().to_owned(),
+                number: number.unwrap(),
+            };
+
+            debug!("{:?}", event);
+            Ok(event)
         } else {
             Err("malformed json")
         }
+    }
+}
+
+mod test {
+    use super::PullRequest;
+
+    #[test]
+    fn test_parse() {
+        let payload = include_str!("pull_request.json");
+        let event = PullRequest::from_str(payload).unwrap();
+        assert_eq!(event.action, "opened");
+        assert_eq!(event.number, 1);
     }
 }
